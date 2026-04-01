2cbd3de6318bbbcbfa67393c79a4f7172a5a1e54
Sandra <lasandry@hotmail.es>
2026-04-01 19:52:21 +0200

    Fix AgendaConfig path

--- sostenibilidad_app/agenda/apps.py
+++ sostenibilidad_app/agenda/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
-
 class AgendaConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'agenda'
+    name = 'sostenibilidad_app.agenda'
+
