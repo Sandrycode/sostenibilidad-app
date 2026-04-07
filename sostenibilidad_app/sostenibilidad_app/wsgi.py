3b18033f2030fee7ea4b1005ac67033c376d5d5f
Sandrycode <lasandry@hotmail.es>
2026-04-07 18:47:33 +0200

    Update wsgi.py

--- sostenibilidad_app/sostenibilidad_app/wsgi.py
+++ sostenibilidad_app/sostenibilidad_app/wsgi.py
@@ -16,4 +16,16 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sostenibilidad_app.settings')
 application = get_wsgi_application()
 
 
+from django.contrib.auth import get_user_model
+User = get_user_model()
+
+if not User.objects.filter(username="sandry_admin").exists():
+    User.objects.create_superuser(
+        username="sandry_admin",
+        email="ejemplo@ejemplo.com",
+        password="Admin1234!"
+    )
+
+
+
 
