d580f68c3c84b25faf1625957541448f8fa99587
Sandrycode <lasandry@hotmail.es>
2026-04-08 11:43:09 +0200

    Update settings.py

--- sostenibilidad_app/sostenibilidad_app/settings.py
+++ sostenibilidad_app/sostenibilidad_app/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-pot)g(m-rx770=h(&+cz8pc(zv*j%+e(rkch9@-cowg$#+_z1w'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
