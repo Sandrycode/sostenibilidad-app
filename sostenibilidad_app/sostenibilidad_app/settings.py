cc090ae46000a560a13f883994585c7e4764964f
Sandrycode <lasandry@hotmail.es>
2026-04-09 20:38:10 +0200

    Update settings.py

--- sostenibilidad_app/sostenibilidad_app/settings.py
+++ sostenibilidad_app/sostenibilidad_app/settings.py
@@ -147,13 +147,13 @@ USE_TZ = True
 STATIC_URL = 'static/'
 import os
 
-# STATICFILES_DIRS = [
-    # BASE_DIR / "sostenibilidad_app" / "agenda" / "static",
-# ]
+ STATICFILES_DIRS = [
+     BASE_DIR / "sostenibilidad_app" / "agenda" / "static",
+ ]
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'agenda/static')
-]
+# STATICFILES_DIRS = [
+    # os.path.join(BASE_DIR, 'agenda/static')
+# ]
 
 
 # Default primary key field type
