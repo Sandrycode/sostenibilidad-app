6a759ee2456035f5e00c68dcb32078b35b745502
Sandrycode <lasandry@hotmail.es>
2026-04-09 18:57:54 +0200

    Update apps.py

--- sostenibilidad_app/diagnostico/apps.py
+++ sostenibilidad_app/diagnostico/apps.py
@@ -2,5 +2,6 @@ from django.apps import AppConfig
 
 class DiagnosticoConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'sostenibilidad_app.diagnostico'
+    name = 'diagnostico'
+
 
