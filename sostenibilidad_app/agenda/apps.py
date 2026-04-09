a04dd336eac2ee6f51b07f7916eaced22c62d627
Sandrycode <lasandry@hotmail.es>
2026-04-09 18:57:03 +0200

    Update apps.py

--- sostenibilidad_app/agenda/apps.py
+++ sostenibilidad_app/agenda/apps.py
@@ -2,5 +2,6 @@ from django.apps import AppConfig
 
 class AgendaConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'sostenibilidad_app.agenda'
+    name = 'agenda'
+
 
