0ca78970cf32e524c2ac4809b7e624abf079b7f3
Sandrycode <lasandry@hotmail.es>
2026-04-10 11:48:24 +0200

    Update settings.py
    refuerzo de la seguridad

--- sostenibilidad_app/sostenibilidad_app/settings.py
+++ sostenibilidad_app/sostenibilidad_app/settings.py
@@ -170,4 +170,9 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
+# REFUERZO DE LA SEGURIDAD
+SECURE_BROWSER_XSS_FILTER = True
+SECURE_CONTENT_TYPE_NOSNIFF = True
+X_FRAME_OPTIONS = 'DENY'
+
 
